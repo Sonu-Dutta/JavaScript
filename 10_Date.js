e0f117a71752953ec95a7e295e89f298929ebc8f
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-06 12:00:45 +0530

    Add files via upload

---
+++ 10_Date.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+// console.log(myDate.getTime()); //in secs
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getHours());
