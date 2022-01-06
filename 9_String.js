d0ef613d1a85d561c757b553b3782abb2bbc27f8
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-06 12:00:28 +0530

    Add files via upload

---
+++ 9_String.js
@@ -0,0 +1,10 @@
+// String Methods in JavaScript
+let myString = "Hello Good Morning and have a Good day ";
+// console.log(myString.length)
+// console.log(myString.indexOf("Good"))
+// console.log(myString.lastIndexOf("Good"))
+
+// console.log(myString.slice(1,4))
+// d = myString.replace("Morning", "Afternoon");
+// d = d.replace("Good", "bad");
+// console.log(d, myString)
