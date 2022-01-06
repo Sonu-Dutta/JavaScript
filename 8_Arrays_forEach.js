6fad2672e75b32b173d8cb7846ec77df62900c44
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-06 12:00:11 +0530

    Add files via upload

---
+++ 8_Arrays_forEach.js
@@ -0,0 +1,8 @@
+// Iteration by forEach-loop
+ var arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+// // const ac = 0;
+// ac++; & ac = ac +1; --->Throws error
+
