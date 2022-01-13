1015b2e990349bed20c501356a575dc4aa78bfc4
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-13 13:03:37 +0530

    Add files via upload

---
+++ 15_LocalStorage_JSON.js
@@ -0,0 +1,19 @@
+// JavaScript localStorage
+// localStorage.setItem('name', 'Sonu') 
+// console.log(localStorage )
+// console.log(localStorage.getItem('name'))
+// localStorage.removeItem('name')
+// localStorage.clear();
+
+// Json 
+obj = {name: "Sonu Dutta", length: 1,a:{"greet": "hello"}}//Json supports double quotes only
+// jso = JSON.stringify(obj);
+// console.log(typeof jso);
+// console.log(typeof obj);
+// console.log(jso)
+// parsed = JSON.parse(`{"name": "Sonu", "length": 1,"a":{"greet": "hello"}}`)
+// console.log(parsed);
+
+// Template literals - Backticks
+a = 50;
+console.log(`The number is ${a}`)
