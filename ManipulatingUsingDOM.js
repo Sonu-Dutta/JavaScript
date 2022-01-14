6f43f7baefe6173a7c276b2497e8eaba02ad376a
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-14 21:39:36 +0530

    Add files via upload

---
+++ ManipulatingUsingDOM.js
@@ -0,0 +1,18 @@
+let x=window;
+let y=window.document;
+console.log(x);
+// window.alert("hello")
+// alert("hi") // without windows you can write alert as window is global variable
+
+// x=prompt("Are you hungry?");
+// console.log(x);
+
+// x=confirm("Are you sure you will sleep now?");
+// console.log(x)
+
+z=window.innerHeight;
+z=innerWidth; //it will work even if you remove window
+z=scrollX; //how much you have scrolled
+z=scrollY;
+z=window.history;
+console.log(z);
