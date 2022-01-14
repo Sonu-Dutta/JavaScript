ed8c60636c3ac93b67b6a383babd16343a62c9f8
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-14 21:38:47 +0530

    Add files via upload

---
+++ Dom_websiteLayout.js
@@ -0,0 +1,12 @@
+// document.getElementById('first').innerHTML="hello";
+let a = document;
+a=document.all;
+// a=document.body;
+// a=document.forms;
+// a=document.links;
+a=document.images;
+// a=document.scripts;
+console.log(a)
+Array.from(a).forEach(function(element){
+    // console.log(element);
+})
