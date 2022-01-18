655a08c587832c8293a40c8f2133cb5bb90b4b75
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-18 19:08:53 +0530

    Add files via upload

---
+++ MathObjects.js
@@ -0,0 +1,33 @@
+console.log("Math Object");
+let x = 13;
+let y = 26;
+let z;
+z = x+y;
+z = x-y;
+z = x*y;
+z = x/y;
+
+// Exploring the Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(15.4);
+z = Math.round(15.7);
+z = Math.ceil(15.3);
+z = Math.ceil(15.8);
+z = Math.floor(15.3);
+z = Math.floor(15.9);
+z = Math.abs(-5);
+z = Math.sqrt(36);
+z = Math.pow(12, 3);
+z = Math.min(2, 23, 134,2374, 24342,34);
+z = Math.max(42, 63, 2334,3234,334);
+z = Math.random();
+z = 100*Math.random(); //random values from 0 to 100
+z = Math.ceil(50 + (100-50)*Math.random());  //random values from 50 to 100
+z = Math.ceil(10 + (100-80)*Math.random());  //random values from  10 to 20
+// a = (0, 1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
+
+console.log(z);
